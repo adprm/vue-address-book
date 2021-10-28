ccaeadbde21152ea042165728af6938256919193
Ariq Heritsa <ariqhm@gmail.com>
2021-10-28 18:54:58 +0700

    add blog

---
+++ src/views/Blog.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Blog</h1>
+    <h4>{{ counter }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Blog',
+  methods: {
+    increment() {
+      counter++;
+    }
+  },
+  data: function () {
+    return {
+      counter: 0,
+    }
+  }
+}
+</script>
